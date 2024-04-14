c61fe551d48e3688d5bf58c5a584672b518932a3
Lovakpoet <daniil_kovalyov@mail.ru>
2024-04-14 18:04:01 +0300

    Substitute-sout-on-logger Added-Connect-CRUD-Hibernate Implemented-CRUD-Hibernate Added-annotations-in-class-User

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,97 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-
+        //was already in the template
     }
 
+    Logger log = Logger.getLogger(this.getClass().getName());
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.sessionFactory.openSession()) {
+            session.beginTransaction();
+            String sqlQuery = """
+                    CREATE TABLE IF NOT EXISTS users (
+                      id INT NOT NULL AUTO_INCREMENT,
+                      name VARCHAR(45) NOT NULL,
+                      lastName VARCHAR(45) NOT NULL,
+                      age INT NOT NULL,
+                      PRIMARY KEY (id))
+                    """;
+            session.createSQLQuery(sqlQuery).executeUpdate();
+            session.getTransaction().commit();
+            log.info( "CREATE TABLE OK!");
+        } catch (Exception e) {
+            log.warning(e + ": (CREATE TABLE FAIL!)");
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = Util.sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            session.getTransaction().commit();
+            log.info( "DROP TABLE OK!");
+        } catch (Exception e){
+            log.warning(e + ": (DROP TABLE FAIL!)");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.sessionFactory.openSession()) {
+            session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            session.getTransaction().commit();
+            log.info("SAVE USER OK!");
+        } catch (Exception e){
+            log.warning(e + ": (SAVE USER FAIL!)");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DELETE FROM users where id = " + id).executeUpdate();
+            session.getTransaction().commit();
+            log.info( "DELETE USER OK!");
+        } catch (Exception e){
+            log.warning(e + ": (DELETE USER FAIL!)");
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.sessionFactory.openSession()) {
+            session.beginTransaction();
+            List<User> list = session.createQuery("from User").getResultList();
+            session.getTransaction().commit();
+            log.info( "GET ALL USERS OK!");
+            return list;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("TRUNCATE TABLE users").executeUpdate();
+            session.getTransaction().commit();
+            log.info( "CLEANING TABLE OK");
+        } catch (Exception e){
+            log.warning(e + ": (CLEANING TABLE FAIL!)");
 
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,32 +1,51 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private Util() {}
+    // Настройка соеденения с БД
+    static Logger log = Logger.getLogger(Util.class.getName());
+
     private static final String DB_URL = "jdbc:mysql://localhost:3306/PP";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "dazvazkamaz1";
 
     public static Connection getConnection() {
         Connection connection = null;
-
         try {
             connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
             if (!connection.isClosed()) {
-                System.out.println("CONNECTED OK!");
-
+                log.info("CONNECTED OK!");
             }
-
         } catch (SQLException e) {
-            System.out.println("CONNECTED FAIL!");
-
+            log.warning(e + ": (CONNECTED FAIL! ");
         }
-
         return connection;
-
     }
+
+    public static final SessionFactory sessionFactory = new Configuration()
+            .addProperties(getPropertiesSessionFactory())
+            .addAnnotatedClass(User.class)
+            .buildSessionFactory();
+
+    private static Properties getPropertiesSessionFactory() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.connection.url", DB_URL);
+        properties.setProperty("dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.setProperty("hibernate.connection.username", DB_USERNAME);
+        properties.setProperty("hibernate.connection.password", DB_PASSWORD);
+        properties.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+        return properties;
+    }
+
 
 }
